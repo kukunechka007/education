29eea3e044761b54609d29a1c9c96fd324b3b22d
Kate <eamyskina@gmail.com>
2024-01-22 17:51:21 +0100

    добавлена четвертая строка для вывода.

---
+++ houmework01/MyLife.java
@@ -0,0 +1,12 @@
+package education.houmework01;
+
+public class MyLife  {
+    static public void main (String[] args)
+    {
+        System.out.println ("Меня зовут Катя.");
+        System.out.println ("Учусь в школе AIT.");
+        System.out.print ("Скоро буду программировать на Java, как большая.");
+        System.out.print ("Изучаю GitHub. Интересная штука.");
+
+    }
+}
